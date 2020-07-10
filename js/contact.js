a1fdbb9e540e3bf2dda921b263ac4bc6478fb1be
bwcsemaj <bwc_semaj@live.com>
2020-07-10 15:03:50 -0400

    adding comments

--- js/contact.js
+++ js/contact.js
@@ -1,4 +1,6 @@
-//Contact Validation
+/**
+ * Validate all contact form elements to make sure they have valid inputs
+ */
 function validateContactForm() {
     let statusElement = document.querySelector(".status");
 
@@ -48,6 +50,10 @@ function validateContactForm() {
 }
 
 //https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+/**
+ * Validate whether the string is a valid email address
+ * @param {string} email 
+ */
 function validateEmail(email) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
